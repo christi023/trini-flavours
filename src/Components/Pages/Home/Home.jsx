4a30b74899decfe727104222f8a41b89b96d6efc
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-12-07 10:28:14 +0100

    Added About component

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // Components
 import Hero from '../../Hero/Hero';
+import About from '../../Pages/About/About';
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 };
