c7a413b50be7581be36d648adb2bc8c31137ff8b
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-12-27 20:50:55 +0100

    Created a why us detail section with styles

---
+++ src/Components/Pages/WhyUs/WhyUs.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+
+// style
+import './WhyUs.css';
+
+const WhyUs = () => {
+  return (
+    <>
+      <section id="why-us" className="why-us">
+        <div className="container">
+          <div className="section-title">
+            <h2>Why Us</h2>
+            <br />
+            <p>Why Choose Trini Flavours</p>
+          </div>
+          <br />
+
+          <div className="row">
+            <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+              <div className="box">
+                <span>
+                  <i className="fas fa-handshake"></i>
+                </span>
+                <h4 className="text-dark">SUPERIOR VALUE </h4>
+                <p>
+                  If you are looking for value for money, Trini Flavours delivers. You will receive
+                  a fresh 100% home made dish in a portion size that won’t leave you scrapping your
+                  plate.
+                </p>
+              </div>
+            </div>
+            <br />
+
+            <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+              <div className="box">
+                <span>
+                  <i className="fas fa-thumbs-up"></i>
+                </span>
+                <h4>100% HOME MADE </h4>
+                <p>
+                  By creating everything from the start, we have 100% control over the ingredients
+                  used, the quality and the freshness of the dish. You will taste the difference.
+                </p>
+              </div>
+            </div>
+            <br />
+
+            <div className="col-lg-4 mt-4 mt-lg-0">
+              <div className="box">
+                <span>
+                  <i className="fas fa-book-open"></i>
+                </span>
+                <h4>FLEXIBLE MENU</h4>
+                <p>
+                  Whether you’re looking to dine with three people or 20 people, we can mix and
+                  match food combinations without a hassle. No one has to fight over the last roti!
+                </p>
+              </div>
+            </div>
+            <br />
+
+            <div className="col-lg-4 mt-4 mt-lg-0">
+              <div className="box">
+                <span>
+                  <i className="fas fa-star "></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                </span>
+                <h4>FIVE STAR RATINGS </h4>
+                <p>
+                  Hungry on a Sunday or a Monday? That’s no problem Trini Flavours is open 7 days a
+                  week.
+                </p>
+              </div>
+            </div>
+            <br />
+
+            <div className="col-lg-4 mt-4 mt-lg-0">
+              <div className="box">
+                <span>
+                  <i className="fas fa-door-open"></i>
+                </span>
+                <h4>OPEN 7 DAYS </h4>
+                <p>
+                  Hungry on a Sunday or a Monday? That’s no problem Trini Flavours is open 7 days a
+                  week.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default WhyUs;
