eb3b50e19fae8376dae704031fe0398f3edd6da9
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-12-27 20:52:53 +0100

    Added Why Us component

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 // Components
 import Hero from '../../Hero/Hero';
 import About from '../../Pages/About/About';
+import WhyUs from '../../Pages/WhyUs/WhyUs';
 
 const Home = () => {
   return (
     <>
       <Hero />
       <About />
+      <WhyUs />
     </>
   );
 };
