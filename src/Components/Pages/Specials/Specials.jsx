3467619aaaa598d9a28df9884a554b2d8024651a
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-12-29 19:45:24 +0100

    Specials component & style css

---
+++ src/Components/Pages/Specials/Specials.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// styles
+import './Specials.css';
+
+const Specials = () => {
+  return (
+    <div>
+      <h1>Specials</h1>
+    </div>
+  );
+};
+
+export default Specials;
