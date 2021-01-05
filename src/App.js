949152504d285aee232cd3e84b6b46c25a58ccde
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-01-05 18:31:30 +0100

    Added specials route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Pages/Home/Home';
 import About from './Components/Pages/About/About';
+import Menu from './Components/Pages/Menu/Menu';
+import Specials from './Components/Pages/Specials/Specials';
+//import Gallery from './Components/Pages/Gallery/Gallery';
 
 // styles
 import './App.css';
@@ -16,6 +19,8 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
+          <Route exact path="/menu" component={Menu} />
+          <Route exact path="/specials" component={Specials} />
         </Switch>
       </Router>
     </>
